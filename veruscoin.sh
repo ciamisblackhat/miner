f137f92022c024e7eabebc89ae280d6a30e15ae5
ciamisblackhat <84260137+ciamisblackhat@users.noreply.github.com>
2021-05-16 11:56:52 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RDN19fU3kdBmeY2twna2NCRY2aKU9hna3C.King -p x --cpu 4
